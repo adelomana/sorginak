bdab73ba5261f6e4407be4a87a9b510492b81750
Adrian de Lomana <alomana@systemsbiology.org>
2018-10-12 16:42:01 -0700

    initial commit

---
+++ barcodeWorker.py
@@ -0,0 +1,10 @@
+###
+### 
+###
+
+barcode='ATTCACTTCAGTCATGAT'
+reverseComplementBarcode='ATCATGACTGAAGTGAAT'
+
+select reads with the barcode in R1 or the reverse complement in R2
+
+generate histograms of read maping for highesta buundace transcripts.
---
+++ main.py
@@ -0,0 +1,9 @@
+# read options
+userDefinedVariables=optionReader.main()
+
+# run algoritm
+central.main()
+
+# generate barcode-specific FASTQ files
+
+# run quantification pipelines for each barcode-specific FASTQ file
